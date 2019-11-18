5b24cd8a169c7e92ab9f5ff038d553354d724c3c
unfa <unfa00@gmail.com>
2019-11-18 23:36:01 +0100

    Update
    Many, many changes!

--- irc2png.sh
+++ irc2png.sh
@@ -1,16 +1,47 @@
 #!/bin/bash
 
-### irc2png - created by Tobiasz 'unfa' Karoń
+VERSION="0.01"
 
-rm -rf temp
+if [[ "$1" = "-v" || "$1" = "--version" ]]; then
+    echo "irc2png version 0.01,
+    Render beautiful animated PNG sequences from IRC logs
+    
+    Created by Tobiasz 'unfa\' Karoń
+    Released under the GPL 3.0 license
+    
+    Get updates and contribute on Github:
+    https://github.com/unfa/irc2png"
+    exit
+fi
 
-mkdir temp
+if [[ "$1" = "" || "$2" = "" || "$1" = "-h" || "$1" = "--help" ]]; then
+    echo "Usage: irc2png [IRC log file] [SVG template file]
+    
+    example:
+    
+        ./irc2png.sh irc.txt template.svg
+        
+    Will render the IRC log from file irc.txt usiing template.svg for visual processing and put the resulting PNG sequence into the subdirectory './irc2png'.
+    
+    Commandline options:
+        
+    -h --help       Display this help screen
+    -v --version    Show version information"
+    exit
+fi
 
-cp irc.txt ./temp
 
-cp chat.svg ./temp
 
-cd temp
+IRC=$1
+SVG=$2
+
+if [[ -e "./irc2png" ]]; then
+    echo "Fatal error! The irc2png directory exists - please delete it or move it to ensure there will be no accidental data loss."
+    exit
+fi
+
+mkdir irc2png
+cd irc2png
 
 TIME8=""; USER8=""; MESSAGE8=""
 TIME7=""; USER7=""; MESSAGE7=""
@@ -22,7 +53,7 @@ TIME2=""; USER2=""; MESSAGE2=""
 TIME1=""; USER1=""; MESSAGE1=""
 TIME="";  USER="";  MESSAGE=""
 
-NOW=$(head -n 1 irc.txt | grep -o "^\[..:..:..\]" | cut -c 2- | rev | cut -c 2- | rev)
+NOW=$(head -n 1 "../$IRC" | grep -o "^\[..:..:..\]" | cut -c 2- | rev | cut -c 2- | rev)
 
 NOW_S=$(date +'%s' -d "$NOW")
 
@@ -43,42 +74,43 @@ while read line; do
         
     # render a frame
     
-    while [[ $NOW_S -lt $TIME_S ]]; do
+    cp "../$SVG" -f template.svg
+    
+    sed -i "s/%TIME1%/$TIME1/" template.svg
+    sed -i "s/%TIME2%/$TIME2/" template.svg
+    sed -i "s/%TIME3%/$TIME3/" template.svg
+    sed -i "s/%TIME4%/$TIME4/" template.svg
+    sed -i "s/%TIME5%/$TIME5/" template.svg
+    sed -i "s/%TIME6%/$TIME6/" template.svg
+    sed -i "s/%TIME7%/$TIME7/" template.svg
+    sed -i "s/%TIME8%/$TIME8/" template.svg
+    
+    sed -i "s/%USER1%/$USER1/" template.svg
+    sed -i "s/%USER2%/$USER2/" template.svg
+    sed -i "s/%USER3%/$USER3/" template.svg
+    sed -i "s/%USER4%/$USER4/" template.svg
+    sed -i "s/%USER5%/$USER5/" template.svg
+    sed -i "s/%USER6%/$USER6/" template.svg
+    sed -i "s/%USER7%/$USER7/" template.svg
+    sed -i "s/%USER8%/$USER8/" template.svg
+    
+    sed -i "s/%MESSAGE1%/$MESSAGE1/" template.svg
+    sed -i "s/%MESSAGE2%/$MESSAGE2/" template.svg
+    sed -i "s/%MESSAGE3%/$MESSAGE3/" template.svg
+    sed -i "s/%MESSAGE4%/$MESSAGE4/" template.svg
+    sed -i "s/%MESSAGE5%/$MESSAGE5/" template.svg
+    sed -i "s/%MESSAGE6%/$MESSAGE6/" template.svg
+    sed -i "s/%MESSAGE7%/$MESSAGE7/" template.svg
+    sed -i "s/%MESSAGE8%/$MESSAGE8/" template.svg
+
+    convert -background none template.svg $(printf "%09d.png" $N)
+        
+    while [[ $NOW_S -lt $TIME_S ]]; do # now we'll duplicate frames over to pad in the space until anotehr message in the log arrives
     
         N=$((N+1))
         NOW_S=$((NOW_S+1))
-
-        cp chat.svg chat2.svg
         
-        sed -i "s/%TIME1%/$TIME1/" chat2.svg
-        sed -i "s/%TIME2%/$TIME2/" chat2.svg
-        sed -i "s/%TIME3%/$TIME3/" chat2.svg
-        sed -i "s/%TIME4%/$TIME4/" chat2.svg
-        sed -i "s/%TIME5%/$TIME5/" chat2.svg
-        sed -i "s/%TIME6%/$TIME6/" chat2.svg
-        sed -i "s/%TIME7%/$TIME7/" chat2.svg
-        sed -i "s/%TIME8%/$TIME8/" chat2.svg
-        
-        sed -i "s/%USER1%/$USER1/" chat2.svg
-        sed -i "s/%USER2%/$USER2/" chat2.svg
-        sed -i "s/%USER3%/$USER3/" chat2.svg
-        sed -i "s/%USER4%/$USER4/" chat2.svg
-        sed -i "s/%USER5%/$USER5/" chat2.svg
-        sed -i "s/%USER6%/$USER6/" chat2.svg
-        sed -i "s/%USER7%/$USER7/" chat2.svg
-        sed -i "s/%USER8%/$USER8/" chat2.svg
-        
-        sed -i "s/%MESSAGE1%/$MESSAGE1/" chat2.svg
-        sed -i "s/%MESSAGE2%/$MESSAGE2/" chat2.svg
-        sed -i "s/%MESSAGE3%/$MESSAGE3/" chat2.svg
-        sed -i "s/%MESSAGE4%/$MESSAGE4/" chat2.svg
-        sed -i "s/%MESSAGE5%/$MESSAGE5/" chat2.svg
-        sed -i "s/%MESSAGE6%/$MESSAGE6/" chat2.svg
-        sed -i "s/%MESSAGE7%/$MESSAGE7/" chat2.svg
-        sed -i "s/%MESSAGE8%/$MESSAGE8/" chat2.svg
-
-        convert -background none chat2.svg chat_$(printf "%09d.png" $N)
-    
+        cp --reflink $(printf "%09d.png" $((N-1))) $(printf "%09d.png" $N) # using --reflink to only copy file reference - saves disk space on Btrfs and ZFS, may not work on other filesystems?)
     done
     
     # step forward with the messages
@@ -92,4 +124,4 @@ while read line; do
     TIME2=$TIME1; USER2=$USER1; MESSAGE2=$MESSAGE1
     TIME1=$TIME; USER1=$USER; MESSAGE1=$MESSAGE
 
-done < irc.txt
+done < "../$IRC"
